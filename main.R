8d62cbe4db289e78f83cf87ed40041b37a4480e9
mingkaijiang <shanyujordan512@gmail.com>
2020-11-02 14:18:40 +1100

    working on plotting script

--- main.R
+++ main.R
@@ -1,53 +1,33 @@
 ### Repository to analyze LPJ-GUESS simulation result for Australia
 ### M.Jiang
 
-####
-require(ggplot2)
-#library(gganimate)
 
+############################# Set up #################################
+#### clear wk space
+rm(list=ls(all=TRUE))
+
+#### read prepare.R
+source("prepare.R")
+
+
+############################# Read input #################################
+### get access to HIE General 2
+### not fully developed yet
+#scp(host="43.240.97.5", 
+#    path="/data/Jiang_M/", 
+#    keypasswd = NA, 
+#    user = "u30046137", rsa = TRUE)
+
+### use local LAI example to develop the plotting script first
 myDF <- read.table("input/run1/lai.out", header=T)
 
+### merge all 20 runs together
 for (i in 2:20) {
     myDF2 <- read.table(paste0("input/run", i, "/lai.out"), header=T)
     myDF <- rbind(myDF, myDF2)
 }
 
 
-
-### plot all LAI over 115 years
-for (i in 1901:2015) {
-    
-    ### subset
-    tDF <- subset(myDF, Year == i)
-    
-    ### plotting
-    p1 <- ggplot() + 
-        geom_tile(data=tDF, aes(y=Lat, x=Lon, fill=Total)) +
-        coord_quickmap(xlim=range(tDF$Lon), ylim=range(tDF$Lat))+
-        borders("world", col="grey", lwd=0.2) +
-        theme(panel.grid.minor=element_blank(),
-              axis.text.x=element_text(size=14),
-              axis.title.x=element_text(size=16),
-              axis.text.y=element_text(size=14),
-              axis.title.y=element_text(size=16),
-              legend.text=element_text(size=10),
-              legend.title=element_text(size=12),
-              panel.grid.major=element_blank(),
-              legend.box = 'vertical',
-              legend.box.just = 'left',
-              legend.position = "bottom",
-              legend.background = element_rect(fill="white",
-                                               size=0.5, linetype="solid", 
-                                               colour ="white"),
-              plot.title = element_text(size=14, face="bold.italic", 
-                                        hjust = 0.5))+
-        scale_fill_continuous(type = "viridis")+
-        ggtitle(i)
-    
-    
-    pdf(paste0("output/lai_", i, ".pdf"), width=8, height=5)
-    plot(p1)
-    dev.off()
-}
-
-
+############################# plotting #################################
+### plot PFT-based response variable change over time
+plot_temporal_PFT_shift(myDF)
---
+++ prepare.R
@@ -0,0 +1,23 @@
+#### Create output folder
+if(!dir.exists("output")) {
+    dir.create("output", showWarnings = FALSE)
+}
+
+#### Install packages
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(dplyr, 
+               doBy, 
+               lubridate,
+               ggplot2,
+               viridis,
+               sciplot,
+               scales,
+               data.table,
+               cowplot,
+               gridExtra,
+               ggthemes)    
+
+
+#### Sourcing all R files in the modules subdirectory
+sourcefiles <- dir("scripts", pattern="[.]R$", recursive = TRUE, full.names = TRUE)
+for(z1 in sourcefiles)source(z1)
