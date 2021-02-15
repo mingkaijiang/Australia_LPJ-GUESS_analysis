3f67daa68e12b65ee896ef06a958027101fff63b
mingkaijiang <shanyujordan512@gmail.com>
2021-02-15 21:07:42 +1100

    end day

--- scripts/plot_animated_individual_map.R
+++ scripts/plot_animated_individual_map.R
@@ -1,5 +1,85 @@
 plot_animated_individual_map <- function(myDF) {
     
+    
+    ### how does each individual evolve within each grid over time
+    test <- subset(myDF, Lon == 150.25 && Lat == -34.25)
+    
+    ### averaging all individual for each year
+    sumDF <- summaryBy(Iage+FPC~Year+PFT, FUN=c(mean, sd), data=test, keep.names=T, na.rm=T)
+    
+    ### plot
+    p1 <- ggplot(test) +
+        geom_point(aes(Iage, FPC, col=Year, fill=Year))+
+        scale_fill_continuous(type = "viridis")+
+        scale_color_continuous(type = "viridis")+
+        theme_linedraw() +
+        theme(panel.grid.minor=element_blank(),
+              axis.title.x = element_text(size=12), 
+              axis.text.x = element_text(size=12),
+              axis.text.y=element_text(size=12),
+              axis.title.y=element_text(size=12),
+              legend.text=element_text(size=6),
+              legend.title=element_text(size=12),
+              panel.grid.major=element_blank(),
+              legend.position="bottom",
+              legend.text.align=0)+
+        ylim(c(0,1))
+    
+    plot(p1)
+    
+    ### to start with, just look at PDF of patch age within each year for all grids
+    for (i in 2005:2015) {
+        test <- subset(myDF, Year == 2005)
+        
+        ### plot Stand NPP vs. Patch age
+        p1 <- ggplot(test) +
+            geom_hex(mapping=aes(x=Page, y=LAI), bins = 50) +
+            scale_fill_continuous(type = "viridis") +
+            #geom_point(faceDF, mapping=aes(x=MAT, y=MAP, 
+            #                               color=factor(Biome),
+            #                               shape=factor(Biome), 
+            #                               size = factor(Biome)),
+            #           inherit.aes = FALSE)+
+            #xlab(expression("MAT (" * degree * "C)")) + 
+            #ylab("MAP (mm)") +
+            #scale_color_manual(name="", 
+            #                   values=col.list2,
+            #                   labels=forests2) +
+            #scale_shape_manual(values=c(17, rep(15, 7)),
+            #                   labels=forests2) +
+            #scale_size_manual(values=c(rep(4, 8)),
+            #                  labels=forests2)+
+            theme_linedraw() +
+            theme(panel.grid.minor=element_blank(),
+                  axis.title.x = element_text(size=12), 
+                  axis.text.x = element_text(size=12),
+                  axis.text.y=element_text(size=12),
+                  axis.title.y=element_text(size=12),
+                  legend.text=element_text(size=6),
+                  legend.title=element_text(size=12),
+                  panel.grid.major=element_blank(),
+                  legend.position="bottom",
+                  legend.text.align=0)#+
+            #guides(size = "none",
+            #       shape = "none",
+            #       #fill = viridis,
+            #       color = guide_legend(ncol=5, override.aes = 
+            #                                list(size = 4, shape=c(17, rep(15, 7)), 
+            #                                     colour=col.list2)))
+        
+        plot(p1)
+            
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     ### conver 0 to NAs
     inDF <- myDF
     inDF[inDF == 0] <- NA
