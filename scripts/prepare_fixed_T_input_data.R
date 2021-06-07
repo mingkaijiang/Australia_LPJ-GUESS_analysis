8ce457d92b4acad8ab99867f7e1ba5f819782801
mingkaijiang <shanyujordan512@gmail.com>
2021-06-07 13:54:11 +1000

    not able to get the binary file right

--- scripts/prepare_fixed_T_input_data.R
+++ scripts/prepare_fixed_T_input_data.R
@@ -4,22 +4,12 @@ prepare_fixed_T_input_data <- function(sourceDir) {
     
     infile.name <- "cruncep_1901_2015.bin"
     
-    myDF <- readBin(paste0(clim.dir, infile.name), "raw", 10e6)
+    #myDF <- readBin(paste0(clim.dir, infile.name), "raw", 10e6)
     
-    test <- t(matrix(myDF, nrow = 12))
-    
-    
+    #test <- t(matrix(myDF, nrow = 12))
     
     
     
     
-    val <- myDF$V2[myDF$V1=="1901"]
-    
-    myDF$V2 <- val
-    
-    out.name <- "co2_1901_2018_fixed.txt"
-    
-    write.table(myDF, paste0(co2.dir, out.name), 
-                sep = "\t", col.names=F, row.names=F)
     
 }
