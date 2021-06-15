b2af1c2f9992b3ffc859c45efc1ddc45b5255fd4
Mingkai Jiang <shanyujordan512@gmail.com>
2021-06-16 09:31:50 +1000

    waiting for new simulation results

--- scripts/check_CO2_effect.R
+++ scripts/check_CO2_effect.R
@@ -161,6 +161,9 @@ check_CO2_effect <- function() {
     plot(p2)
     dev.off()
     
+    
+    
+    ########################################################################
     ### prepare grid ID
     gridDF <- subset(myDF.lai, Year == "1901")
     gridDF$GridID <- c(1:length(gridDF$Lon))
@@ -639,329 +642,26 @@ check_CO2_effect <- function() {
               combined_plot, base_width=20, base_height = 20)
     
     
+    ########################################################################
+    
+    ### simulation list
+    sim.list <- paste0("run", c(1:20))
+    
+    ### files under variable climate forcing
+    indiv.files1 <- paste0("input/withfire/run", c(1:20), "/indiv.out")
+    indiv.files2 <- paste0("input/run", c(1:20), "/fixCO2varT/indiv.out")
+    
+    ### read input files
+    myDF1 = indiv.files1 %>% 
+        purrr::map_df(~read.table(.,header=T))
+    
+    myDF2 = indiv.files2 %>% 
+        purrr::map_df(~read.table(.,header=T))
     
     
-    ### PFT specific patterns
-    ### read in individual output dataset
-        
-        
-        ### get lon and lat list
-        lon.list <- unique(nofireDF$Lon)
-        lat.list <- unique(nofireDF$Lat)
-        
-        
-        ### merge individual PFT
-        for (j in pft.list2) {
-            plotDF <- merge(myDF[,c("Lon", "Lat", "Year", paste0(j))], 
-                            fireDF[,c("Lon", "Lat", "Year", paste0(j))],
-                            by=c("Lon", "Lat", "Year"))
-            colnames(plotDF) <- c("Lon", "Lat", "Year", "NoCompetition", "Competition")
-            plotDF <- merge(plotDF, nofireDF[,c("Lon", "Lat", "Year", paste0(j))], 
-                            by=c("Lon", "Lat", "Year"))
-            colnames(plotDF) <- c("Lon", "Lat", "Year", "NoCompetition", "Competition",
-                                  "NoFire")
-            
-            
-            ### convert all 0s to NAs
-            plotDF$NoCompetition <- ifelse(plotDF$NoCompetition == 0, NA, plotDF$NoCompetition)
-            plotDF$Competition <- ifelse(plotDF$Competition == 0, NA, plotDF$Competition)
-            plotDF$NoFire <- ifelse(plotDF$NoFire == 0, NA, plotDF$NoFire)
-            
-            plotDF$Competition_Diff <- with(plotDF, NoCompetition - Competition)
-            plotDF$Fire_Diff <- with(plotDF, NoFire - Competition)
-            
-            ### average all years
-            sumDF <- summaryBy(NoCompetition+Competition+NoFire+Competition_Diff+Fire_Diff~Lon+Lat, 
-                               FUN=mean, data=plotDF, na.rm=T, keep.names=T)
-            
-            sumDF$ID <- c(1:max(dim(sumDF)[1]))
-            idDF <- sumDF[,c("Lon", "Lat", "ID")]
-            plotDF <- merge(plotDF, idDF, by=c("Lon", "Lat"))
-            
-            xlim.range <- range(sumDF$Lon)
-            ylim.range <- range(sumDF$Lat)
-            
-            
-            ### set scales
-            plot.scale.range <- range(c(sumDF$NoCompetition, sumDF$Competition, sumDF$NoFire), na.rm=T)
-            plot.scale.range[1] <- ifelse(plot.scale.range[1] > 0, 0.01, plot.scale.range[1])
-            log.plot.scale.range <- log(plot.scale.range)
-            log.plot.scale.range[1] <- log(0.01)
-            log.plot.scale.breaks <- seq(log.plot.scale.range[1], log.plot.scale.range[2], length.out=5)
-            plot.scale.breaks <- round(exp(log.plot.scale.breaks),2)
-            plot.scale.range[2] <- ifelse(plot.scale.range[2] == 0.0, plot.scale.range[2], plot.scale.range[2] + 1.0)
-            
-            
-            #### obtain gridded linear fit relationships
-            #for (k in unique(idDF$ID)) {
-            #    test <- subset(plotDF, ID==k)
-            #    lm1 <- lm(NoCompetition~Year, data=test)
-            #    lm2 <- lm(Competition~Year, data=test)
-            #    lm3 <- lm(NoFire~Year, data=test)
-            #    lm4 <- lm(Competition_Diff~Year, data=test)
-            #    lm5 <- lm(Fire_Diff~Year, data=test)
-            #    
-            #    sumDF$NoCompetition_slope[sumDF$ID==k] <- coef(lm1)[2]
-            #    sumDF$NoCompetition_p[sumDF$ID==k] <- summary(lm1)$coefficients[,4][2]
-            #    
-            #    sumDF$Competition_slope[sumDF$ID==k] <- coef(lm2)[2]
-            #    sumDF$Competition_p[sumDF$ID==k] <- summary(lm2)$coefficients[,4][2]
-            #    
-            #    sumDF$NoFire_slope[sumDF$ID==k] <- coef(lm3)[2]
-            #    sumDF$NoFire_p[sumDF$ID==k] <- summary(lm3)$coefficients[,4][2]
-            #    
-            #    sumDF$Competition_Diff_slope[sumDF$ID==k] <- coef(lm4)[2]
-            #    sumDF$Competition_Diff_p[sumDF$ID==k] <- summary(lm4)$coefficients[,4][2]
-            #    
-            #    sumDF$Fire_Diff_slope[sumDF$ID==k] <- coef(lm5)[2]
-            #    sumDF$Fire_Diff_p[sumDF$ID==k] <- summary(lm5)$coefficients[,4][2]
-            #    
-            #}
-            #
-            #
-            ##### write
-            #saveRDS(sumDF, file=paste0("output/competition/Effect_of_competition_", 
-            #                           i, "_", j, "_all_yr.rds"))
-            
-            
-            ### Plot 100-yr average results
-            p1 <- ggplot() + 
-                geom_tile(data=sumDF, aes(y=Lat, x=Lon, fill=NoCompetition)) +
-                coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-                borders("world", col="grey", lwd=0.2) +
-                theme(panel.grid.minor=element_blank(),
-                      axis.text.x=element_blank(),
-                      axis.title.x=element_blank(),
-                      axis.text.y=element_blank(),
-                      axis.title.y=element_blank(),
-                      legend.text=element_text(size=10),
-                      legend.title=element_text(size=12),
-                      panel.grid.major=element_blank(),
-                      legend.position = "right")+
-                scale_fill_continuous(name=paste0("LAI ", j),
-                                      na.value = 'white',
-                                      trans="log",
-                                      limits = plot.scale.range,
-                                      breaks = plot.scale.breaks,
-                                      labels = plot.scale.breaks,
-                                      type = "viridis")+
-                ggtitle("No competition")
-            
-            
-            p2 <- ggplot() + 
-                geom_tile(data=sumDF, aes(y=Lat, x=Lon, fill=Competition)) +
-                coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-                borders("world", col="grey", lwd=0.2) +
-                theme(panel.grid.minor=element_blank(),
-                      axis.text.x=element_blank(),
-                      axis.title.x=element_blank(),
-                      axis.text.y=element_blank(),
-                      axis.title.y=element_blank(),
-                      legend.text=element_text(size=10),
-                      legend.title=element_text(size=12),
-                      panel.grid.major=element_blank(),
-                      legend.position = "right")+
-                scale_fill_continuous(name=paste0("LAI ", j),
-                                      na.value = 'white',
-                                      trans="log",
-                                      limits = plot.scale.range,
-                                      breaks = plot.scale.breaks,
-                                      labels = plot.scale.breaks,
-                                      type = "viridis")+
-                ggtitle("With competition")
-            
-            
-            p3 <- ggplot() + 
-                geom_tile(data=sumDF, aes(y=Lat, x=Lon, fill=NoFire)) +
-                coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-                borders("world", col="grey", lwd=0.2) +
-                theme(panel.grid.minor=element_blank(),
-                      axis.text.x=element_blank(),
-                      axis.title.x=element_blank(),
-                      axis.text.y=element_blank(),
-                      axis.title.y=element_blank(),
-                      legend.text=element_text(size=10),
-                      legend.title=element_text(size=12),
-                      panel.grid.major=element_blank(),
-                      legend.position = "right")+
-                scale_fill_continuous(name=paste0("LAI ", j),
-                                      na.value = 'white',
-                                      trans="log",
-                                      limits = plot.scale.range,
-                                      breaks = plot.scale.breaks,
-                                      labels = plot.scale.breaks,
-                                      type = "viridis")+
-                ggtitle("No Fire")
-            
-            
-            p4 <- ggplot() + 
-                geom_tile(data=sumDF, aes(y=Lat, x=Lon, fill=Competition_Diff)) +
-                coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-                borders("world", col="grey", lwd=0.2) +
-                theme(panel.grid.minor=element_blank(),
-                      axis.text.x=element_blank(),
-                      axis.title.x=element_blank(),
-                      axis.text.y=element_blank(),
-                      axis.title.y=element_blank(),
-                      legend.text=element_text(size=10),
-                      legend.title=element_text(size=12),
-                      panel.grid.major=element_blank(),
-                      legend.position = "right")+
-                scale_fill_continuous(name=paste0("LAI ", j),
-                                      na.value = 'white',
-                                      type = "viridis")+
-                ggtitle("No Competition - Competition")
-            
-            
-            p5 <- ggplot() + 
-                geom_tile(data=sumDF, aes(y=Lat, x=Lon, fill=Fire_Diff)) +
-                coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-                borders("world", col="grey", lwd=0.2) +
-                theme(panel.grid.minor=element_blank(),
-                      axis.text.x=element_blank(),
-                      axis.title.x=element_blank(),
-                      axis.text.y=element_blank(),
-                      axis.title.y=element_blank(),
-                      legend.text=element_text(size=10),
-                      legend.title=element_text(size=12),
-                      panel.grid.major=element_blank(),
-                      legend.position = "right")+
-                scale_fill_continuous(name=paste0("LAI ", j),
-                                      na.value = 'white',
-                                      type = "viridis")+
-                ggtitle("No Fire - Fire")
-            
-            ## save plot
-            combined_plot <- plot_grid(p1, p2, p3, p4, p5,
-                                       ncol=3, align="vh", axis = "l")
-            
-            save_plot(paste0("output/competition/Effect_of_competition_", i, "_", j, ".pdf"),
-                      combined_plot, base_width=10, base_height = 8)
-            
-            
-            ###################### 
-            ### plot temporal patterns
-            
-            
-            #### make NAs
-            #sumDF2 <- sumDF
-            #sumDF2$NoCompetition_slope <- ifelse(sumDF2$NoCompetition_p <= 0.05, sumDF2$NoCompetition_slope, NA)
-            #sumDF2$Competition_slope <- ifelse(sumDF2$Competition_p <= 0.05, sumDF2$Competition_slope, NA)
-            #sumDF2$NoFire_slope <- ifelse(sumDF2$NoFire_p <= 0.05, sumDF2$NoFire_slope, NA)
-            #sumDF2$Competition_Diff_slope <- ifelse(sumDF2$Competition_Diff_p <= 0.05, sumDF2$Competition_Diff_slope, NA)
-            #sumDF2$Fire_Diff_slope <- ifelse(sumDF2$Fire_Diff_p <= 0.05, sumDF2$Fire_Diff_slope, NA)
-            #
-            #if (sum(sumDF2$NoCompetition_slope, na.rm=T) == 0.0) {
-            #    next
-            #} else {
-            #    ### plot
-            #    p1 <- ggplot() + 
-            #        geom_tile(data=sumDF2, aes(y=Lat, x=Lon, fill=NoCompetition_slope)) +
-            #        coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-            #        borders("world", col="grey", lwd=0.2) +
-            #        theme(panel.grid.minor=element_blank(),
-            #              axis.text.x=element_blank(),
-            #              axis.title.x=element_blank(),
-            #              axis.text.y=element_blank(),
-            #              axis.title.y=element_blank(),
-            #              legend.text=element_text(size=10),
-            #              legend.title=element_text(size=12),
-            #              panel.grid.major=element_blank(),
-            #              legend.position = "right")+
-            #        scale_fill_continuous(name=paste0("LAI slope ", j),
-            #                              na.value = 'white',
-            #                              type = "viridis")+
-            #        ggtitle("No Competition slope")
-            #    
-            #    
-            #    p2 <- ggplot() + 
-            #        geom_tile(data=sumDF2, aes(y=Lat, x=Lon, fill=Competition_slope)) +
-            #        coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-            #        borders("world", col="grey", lwd=0.2) +
-            #        theme(panel.grid.minor=element_blank(),
-            #              axis.text.x=element_blank(),
-            #              axis.title.x=element_blank(),
-            #              axis.text.y=element_blank(),
-            #              axis.title.y=element_blank(),
-            #              legend.text=element_text(size=10),
-            #              legend.title=element_text(size=12),
-            #              panel.grid.major=element_blank(),
-            #              legend.position = "right")+
-            #        scale_fill_continuous(name=paste0("LAI slope ", j),
-            #                              na.value = 'white',
-            #                              type = "viridis")+
-            #        ggtitle("Competition slope")
-            #    
-            #    
-            #    p3 <- ggplot() + 
-            #        geom_tile(data=sumDF2, aes(y=Lat, x=Lon, fill=NoFire_slope)) +
-            #        coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-            #        borders("world", col="grey", lwd=0.2) +
-            #        theme(panel.grid.minor=element_blank(),
-            #              axis.text.x=element_blank(),
-            #              axis.title.x=element_blank(),
-            #              axis.text.y=element_blank(),
-            #              axis.title.y=element_blank(),
-            #              legend.text=element_text(size=10),
-            #              legend.title=element_text(size=12),
-            #              panel.grid.major=element_blank(),
-            #              legend.position = "right")+
-            #        scale_fill_continuous(name=paste0("LAI slope ", j),
-            #                              na.value = 'white',
-            #                              type = "viridis")+
-            #        ggtitle("No Fire slope")
-            #    
-            #    
-            #    p4 <- ggplot() + 
-            #        geom_tile(data=sumDF2, aes(y=Lat, x=Lon, fill=Competition_Diff_slope)) +
-            #        coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-            #        borders("world", col="grey", lwd=0.2) +
-            #        theme(panel.grid.minor=element_blank(),
-            #              axis.text.x=element_blank(),
-            #              axis.title.x=element_blank(),
-            #              axis.text.y=element_blank(),
-            #              axis.title.y=element_blank(),
-            #              legend.text=element_text(size=10),
-            #              legend.title=element_text(size=12),
-            #              panel.grid.major=element_blank(),
-            #              legend.position = "right")+
-            #        scale_fill_continuous(name=paste0("LAI slope ", j),
-            #                              na.value = 'white',
-            #                              type = "viridis")+
-            #        ggtitle("Competition Difference slope")
-            #    
-            #    
-            #    p5 <- ggplot() + 
-            #        geom_tile(data=sumDF2, aes(y=Lat, x=Lon, fill=Fire_Diff_slope)) +
-            #        coord_quickmap(xlim=xlim.range, ylim=ylim.range)+
-            #        borders("world", col="grey", lwd=0.2) +
-            #        theme(panel.grid.minor=element_blank(),
-            #              axis.text.x=element_blank(),
-            #              axis.title.x=element_blank(),
-            #              axis.text.y=element_blank(),
-            #              axis.title.y=element_blank(),
-            #              legend.text=element_text(size=10),
-            #              legend.title=element_text(size=12),
-            #              panel.grid.major=element_blank(),
-            #              legend.position = "right")+
-            #        scale_fill_continuous(name=paste0("LAI slope ", j),
-            #                              na.value = 'white',
-            #                              type = "viridis")+
-            #        ggtitle("Fire Difference slope")
-            #    
-            #    
-            #    ## save plot
-            #    combined_plot <- plot_grid(p1, p2, p3, p4, p5,
-            #                               ncol=3, align="vh", axis = "l")
-            #    
-            #    save_plot(paste0("output/competition/Effect_of_competition_temporal_slope_", i, "_", j, ".pdf"),
-            #              combined_plot, base_width=10, base_height = 8)
-            #}
-            
-            
-            
-            
-        } ## pft.list, j
+    ### merge dataset
+    myDF.lai <- merge(myDF1.lai, myDF2.lai, by=c("Lon", "Lat", "Year"))
+    myDF.cpool <- merge(myDF1.cpool, myDF2.cpool, by=c("Lon", "Lat", "Year"))
         
         
 
